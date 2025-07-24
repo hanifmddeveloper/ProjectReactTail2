1014e8f3e0ab11e860b50eed1999b853832466c2
hanifmddeveloper <hanifmddeveloper@gmail.com>
2025-07-24 19:54:12 +0600

    add map width

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,7 +6,7 @@ const Map = () => {
    <section className='' >
     <Container>
      <div className='' >
-       <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d58422.56651762437!2d90.35048899965489!3d23.768395589301043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x3755c75e87cc6ceb%3A0x4099f6cd0d017b2f!2sPrime%20Minister&#39;s%20Office%2C%20Old%20Parliament%20Building%2C%20Dacca!3m2!1d23.7683187!2d90.3916335!4m5!1s0x3755c75e87cc6ceb%3A0x4099f6cd0d017b2f!2sQ95R%2BWGG%20Prime%20Minister&#39;s%20Office%2C%20Old%20Parliament%20Building%2C%20Dhaka%201221!3m2!1d23.7683187!2d90.3916335!5e0!3m2!1sen!2sbd!4v1753205030039!5m2!1sen!2sbd" width="1363px" height="450" ></iframe>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d58422.56651762437!2d90.35048899965489!3d23.768395589301043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x3755c75e87cc6ceb%3A0x4099f6cd0d017b2f!2sPrime%20Minister&#39;s%20Office%2C%20Old%20Parliament%20Building%2C%20Dacca!3m2!1d23.7683187!2d90.3916335!4m5!1s0x3755c75e87cc6ceb%3A0x4099f6cd0d017b2f!2sQ95R%2BWGG%20Prime%20Minister&#39;s%20Office%2C%20Old%20Parliament%20Building%2C%20Dhaka%201221!3m2!1d23.7683187!2d90.3916335!5e0!3m2!1sen!2sbd!4v1753205030039!5m2!1sen!2sbd" width="1920px" height="450" ></iframe>
      </div>
     </Container>
    </section>
